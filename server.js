51fb6d1e1f5c8d83e3f7181cc7bfd26c3afb5613
renanbmello <renanbmello@gmail.com>
2024-04-17 15:04:21 -0300

    upadate delete function

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const DB_URI = process.env.DB_URI
 
-// Middleware para parsear o corpo das requisições como JSON
+
 app.use(bodyParser.json());
+app.use(cors())
 
-// Conectar ao banco de dados MongoDB
-mongoose.connect('mongodb://localhost:27017/minhaapp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect(DB_URI)
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erro de conexão com o MongoDB:'));
 db.once('open', () => console.log('Conectado ao MongoDB'));
 
-// Definir o esquema e modelo para os itens
+
 const itemSchema = new mongoose.Schema({
   name: String,
   value: Number,
 });
 const Item = mongoose.model('Item', itemSchema);
 
-// Rotas para manipular os itens
+
 app.get('/items', async (req, res) => {
   try {
     const items = await Item.find({});
@@ -40,6 +44,17 @@ app.post('/items', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Erro ao adicionar item.' });
+  }
+});
+
+app.delete('/items/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Item.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Item excluído com sucesso.' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Erro ao excluir item.' });
   }
 });
 
